b66b5b6774bed79fb48e0c9314416418dfccd27e
Rikki Prince <rfp@ecs.soton.ac.uk>
2016-12-08 20:13:44 +0000

    integration

--- game_loop.js
+++ game_loop.js
@@ -1,28 +1,35 @@
 var ONE_FRAME_TIME = 1000 / 60 ;
 var snowflakes = [];
 var width = 500;
-var fractionOfWidth = 0.25;
+var fractionOfWidth = 0.01;
+var howManyNew = 1;
 
 function generateNewSnowflakes() {
-	for(var i=0; i<(width*fractionOfWidth); i++) {
+	for(var i=0; i<(howManyNew); i++) {
 		snowflakes.push(new Particle(Math.random()*width,0));
 	}
 }
 
+var loopCount = 0;
 function main_loop() {
-
+	//console.log("main loop");
+	// update
 	for(var i=0; i<snowflakes.length; i++) {
 		snowflakes[i].update();
 	}
 
-	for(var j=0; i<snowflakes.length; i++) {
-		draw(snowflake);
-	}
+	// draw
+	draw(snowflakes);
 	generateNewSnowflakes();
 
-	setInterval( main_loop, ONE_FRAME_TIME );
+	if(loopCount < 20) {
+		setTimeout( main_loop, ONE_FRAME_TIME );
+		loopCount++;
+		console.log(loopCount);
+	}
 }
 
 function start_loop() {
-	setInterval( main_loop, ONE_FRAME_TIME );
+	//console.log("start");
+	setTimeout( main_loop, ONE_FRAME_TIME );
 }
